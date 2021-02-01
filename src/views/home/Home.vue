68d8dd292bfffa0ffab299c72141d32094c44757
liuf <liufengduke@163.com>
2021-02-01 15:24:50 +0800

    首页FeatureView展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,7 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <FeatureView/>
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import NavBar from "@/components/common/navbar/NavBar";
 import {getHomeMultidata} from "@/network/home";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
 export default {
   name: "Home",
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
@@ -29,8 +32,7 @@ export default {
   created() {
     //1.请求多个数据
     getHomeMultidata().then(res => {
-      console.log(res);
-      //保存到result变量中
+      // 保存到result变量中
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
     })
@@ -39,9 +41,18 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
+
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  z-index: 9;
 }
 
 </style>
